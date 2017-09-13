313b77f817050a88da59047dcbb0a1c4bd52bbc8
Stanley <snicholl@gmail.com>
2017-09-13 15:53:49 -0700

    updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,119 @@
-// Tasks:
 
+// Tasks:
 // document.getElementById
 // Find just the title
+var title = document.getElementById('title')
+//JS is looking for an event to happen, in this case a click. When that click happens, it does something (launches a function)
+
+var colors = ['red','orange','yellow','green','blue','indigo','purple']
+var count = 0
+title.addEventListener('click', function(){
+  console.log('you did it!!')
+  title.style.color = colors[count]
+
+  if(count === colors.length-1){
+    count = 0
+  }else {
+    count++
+  }
+})
+
 // Change the text of the title
+// - Choose Your Magical Ingredients
+title.textContent = 'Choose Your Magical Ingredients'
+// title.innerHTML = 'Choose Your <b>Magical Ingredients</b>'
 // Change the style of the title
+// Overriding styles
+// title.style = 'background-color: purple;'
+// title.style = 'color: orange;'
+
+// Setting specific styles
+// title.style.backgroundColor = 'purple'
+// title.style.color = 'orange'
 // Add a class to the title
+// - Add a class of .princess-font to the title
+title.className = 'princess-font'
+// Add more classes with `+=` and a space
+// title.className += ' highlighted'
 
 // document.querySelectorAll
 // Find all the list items
+var allListItems = document.querySelectorAll('li')
+var selectedItemBox = document.querySelector('.alert span')
+var selectedItems = []
+var counter = 0
+var items = ''
+
+var lis = document.querySelectorAll('li')
+
+for(var i = 0; i < lis.length; i++){
+  var li = lis[i]
+  li.addEventListener('click', function(event){
+    event.target.className += ' selected'
+    event.target.style.color = 'pink'
+    if(selectedItems.length > 4){
+      selectedItems.shift()
+      selectedItems.push(event.target.innerText)
+    }else{
+      selectedItems.push(event.target.innerText)
+    }
+selectedItemBox.textContent = ''
+    for(let j = 0; j < selectedItems.length; j++){
+      selectedItemBox.textContent += ` ${j+1}) ${selectedItems[j]}`
+    }
+  })
+
+}
+var mixButton = document.querySelector('.mix-btn')
+var mix = function(event){
+  var mixer = document.createElement('h3')
+  mixer.textContent = 'You made a potion!'
+  mixer.style.color = 'white'
+  mixer.style.textAlign = 'center'
+  mixer.style.fontWeight = 'bold'
+  mixer.style.marginTop = '2rem'
+  var mixerSection = document.getElementsByClassName('mix-section')
+  console.log(mixerSection);
+  mixerSection[0].append(mixer)
+  selectedItemBox.textContent = 'Nothing is selected yet'
+  selectedItems = []
+}
+
+mixButton.addEventListener('click', mix)
+
+
+
+
+
+
 // Find all list items of a specific list
+// var plants = document.querySelector('.plants')
 // Add a class of .green to all .plants list items
+var plants = document.querySelectorAll('.plants li i')
+for (var i = 0; i < plants.length; i++) {
+  plants[i].className += ' green'
+}
 // Add a class of .blue to all .liquids list items
+var liquids = document.querySelectorAll('.liquids li i')
+for (var i = 0; i < liquids.length; i++) {
+  liquids[i].className += ' blue'
+}
 // Add a class of .red to all .animal-parts list items
+var animalParts = document.querySelectorAll('.animal-parts li i')
+for (var i = 0; i < animalParts.length; i++) {
+  animalParts[i].className += ' red'
+}
 
 // document.createElement
 // Create a new ingredient from scratch!
+// var li = document.createElement('li')
+// li.textContent = 'Eye of Newt'
 // Add it to one of the lists at the top
+// var ul = document.querySelector('ul')
+// ul.prepend(li)
 // Add it to one of the lists at the bottom
+// ul.append(li)
 // Add it to one of the lists in the middle
-
 // element.remove()
 // Find and remove specific items
+// document.querySelectorAll('li')[8].remove()
