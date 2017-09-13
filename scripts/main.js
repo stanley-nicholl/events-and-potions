fcc6f6fb803a1b5d8d4ac3ceb7337236afc9fec4
Wes Reid <bwreid@gmail.com>
2017-09-13 10:36:38 -0700

    Add exercises

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+// Tasks:
+
+// document.getElementById
+// Find just the title
+// Change the text of the title
+// Change the style of the title
+// Add a class to the title
+
+// document.querySelectorAll
+// Find all the list items
+// Find all list items of a specific list
+// Add a class of .green to all .plants list items
+// Add a class of .blue to all .liquids list items
+// Add a class of .red to all .animal-parts list items
+
+// document.createElement
+// Create a new ingredient from scratch!
+// Add it to one of the lists at the top
+// Add it to one of the lists at the bottom
+// Add it to one of the lists in the middle
+
+// element.remove()
+// Find and remove specific items
